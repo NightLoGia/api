c7b0eac868133b160aac8f2294cf9823fd2cb380
NightLoGia <bank.bunditt@gmail.com>
2016-11-26 15:54:18 +0700

    simple server

---
+++ server.go
@@ -0,0 +1,16 @@
+  package main
+  import (
+	"net/http"
+	"github.com/labstack/echo"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+  // Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
